3385e7d797b8bbb54581d8f0f8de00abb1557290
Duc <nguyenducchu1999@gmail.com>
2022-06-26 19:20:57 +0700

    big refactor

--- skills_dashboard/skills/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SkillsConfig(AppConfig):
-    name = 'skills'
--- skills_dashboard/web/src/config.js
+++
@@ -1,1 +0,0 @@
-export const baseUrl = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000' : ''
