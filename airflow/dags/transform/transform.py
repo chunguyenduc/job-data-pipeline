3362523931a77659c81ff3092d4835ac4d73a159
Duc <nguyenducchu1999@gmail.com>
2022-07-20 01:20:50 +0700

    update transform company

--- airflow/dags/transform/transform.py
+++ airflow/dags/transform/transform.py
@@ -1,11 +1,23 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import monotonically_increasing_id, col
 
 spark = SparkSession.builder.appName("Transform").getOrCreate()
 
 df = spark.read.option("header", True).csv(
-    "hdfs://namenode:9000/user/root/job/job-190722-1240.csv"
+    "hdfs://namenode:9000/user/root/job/job-190722-1752.csv"
 )
 df.show(n=20, truncate=True)
 df.printSchema()
 
 df.select("skills").show(n=5, truncate=False)
+
+company_df = df.select(
+    [
+        monotonically_increasing_id().alias("company_id"),
+        col("company").alias("company_name"),
+        col("city"),
+    ]
+).distinct()
+
+company_df.explain()
+company_df.show(truncate=False)
