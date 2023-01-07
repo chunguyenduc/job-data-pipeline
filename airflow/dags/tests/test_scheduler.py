77a08d9c3f2b9e9f541bebb907b8365db518712e
chunguyenduc <nguyenducchu1999@gmail.com>
2023-01-07 21:18:42 +0700

    update test_scheduler.py: clean log

--- airflow/dags/tests/test_scheduler.py
+++ airflow/dags/tests/test_scheduler.py
@@ -11,7 +11,6 @@ class TestScheduler(unittest.TestCase):
     def test_dag_loaded(self):
         dag_bag = DagBag(dag_folder='.')
         dag = dag_bag.get_dag(dag_id="job_data_pipeline")
-        print('DAG: ', dag)
         self.assertTrue(len(dag_bag.import_errors) == 0)
         self.assertTrue(dag is not None)
         self.assertTrue(len(dag.tasks) == 13)
