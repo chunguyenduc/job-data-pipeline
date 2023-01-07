171575a6f3f3c42466ab4a6e830e91160ee93787
chunguyenduc <nguyenducchu1999@gmail.com>
2023-01-08 00:43:26 +0700

    update unittest.sh: update data-file

--- airflow/unittest.sh
+++ airflow/unittest.sh
@@ -6,5 +6,5 @@ echo $BASEDIR
 cd $BASEDIR/dags
 MODULE_LIST=`find . -type f \( -iname "*.py" ! -iname "*__init__.py" ! -iname "*test_*.py" ! -iname "*config*.py" \)`
 ls -l
-coverage run -m unittest discover
-coverage report $MODULE_LIST
+coverage run --data-file tests/.coverage -m unittest discover
+coverage report --data-file tests/.coverage $MODULE_LIST
