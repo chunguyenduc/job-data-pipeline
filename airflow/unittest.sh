4eaeb088fef650e83c37c6d108faf8d2991e22d2
chunguyenduc <nguyenducchu1999@gmail.com>
2023-01-08 00:01:40 +0700

    update unittest.sh: update coverage report

--- airflow/unittest.sh
+++ airflow/unittest.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-
+set -x
 BASEDIR=${PWD}
+MODULE_LIST=find . -type f \( -iname "*.py" ! -iname "*__init__.py" ! -iname "*test_*.py" ! -iname "*config*.py" \)
 echo $BASEDIR
 cd $BASEDIR/dags
 ls -l
-python3 -m unittest discover -v
-coverage run -m unittest discover && coverage report
+coverage run -m unittest discover
+coverage report $(MODULE_LIST)
