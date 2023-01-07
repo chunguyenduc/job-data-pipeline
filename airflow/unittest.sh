9e7f14aef604b189a958df15b8b10d611289e405
chunguyenduc <nguyenducchu1999@gmail.com>
2023-01-08 01:39:57 +0700

    update unittest.sh: try add sudo

--- airflow/unittest.sh
+++ airflow/unittest.sh
@@ -6,6 +6,6 @@ echo $BASEDIR
 cd $BASEDIR/dags
 MODULE_LIST=`find . -type f \( -iname "*.py" ! -iname "*__init__.py" ! -iname "*test_*.py" ! -iname "*config*.py" \)`
 ls -l
-chmod -R a+X .
+sudo chmod -R a+X .
 coverage run -m unittest discover
 coverage report $MODULE_LIST
