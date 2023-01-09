d78d19ae5bbe006b503c01d514b431e2ad9c5b8f
chunguyenduc <nguyenducchu1999@gmail.com>
2023-01-09 19:31:19 +0700

    debug coverage

--- airflow/unittest.sh
+++ airflow/unittest.sh
@@ -3,7 +3,10 @@
 su -l airflow
 set -x
 BASEDIR=${PWD}
+MODULE_LIST=`find . -type f \( -iname "*.py" ! -iname "*__init__.py" ! -iname "*test_*.py" ! -iname "*config*.py" \)`
 cd $BASEDIR/dags
 ls -l
 python3 -m unittest discover -v
 python3 -m coverage run -m unittest
+ls -l
+python3 -m coverage report $(MODULE_LIST)
