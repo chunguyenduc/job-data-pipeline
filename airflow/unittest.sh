feef25a6e84a0be12a7157b5cc3bdb72fc024e1d
chunguyenduc <nguyenducchu1999@gmail.com>
2023-01-09 19:35:21 +0700

    try chmod

--- airflow/unittest.sh
+++ airflow/unittest.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-su -l airflow
 set -x
 BASEDIR=${PWD}
 MODULE_LIST=`find . -type f \( -iname "*.py" ! -iname "*__init__.py" ! -iname "*test_*.py" ! -iname "*config*.py" \)`
@@ -9,4 +8,5 @@ ls -l
 python3 -m unittest discover -v
 python3 -m coverage run -m unittest
 ls -l
+chmod u+x /opt/airflow/dags/.coverage
 python3 -m coverage report $(MODULE_LIST)
