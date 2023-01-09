5de6538e553357bca644764c45c15e43e9412447
chunguyenduc <nguyenducchu1999@gmail.com>
2023-01-09 20:17:58 +0700

    revert unittest

--- airflow/unittest.sh
+++ airflow/unittest.sh
@@ -4,10 +4,5 @@ set -x
 BASEDIR=${PWD}
 MODULE_LIST=`find . -type f \( -iname "*.py" ! -iname "*__init__.py" ! -iname "*test_*.py" ! -iname "*config*.py" \)`
 cd $BASEDIR/dags
-ls -l
-python3 -m unittest discover -v
-touch .coverage
-python3 -m coverage run -m unittest
 ls -l -a
-chmod u+x .coverage
-python3 -m coverage report $MODULE_LIST
+python3 -m unittest discover -v
